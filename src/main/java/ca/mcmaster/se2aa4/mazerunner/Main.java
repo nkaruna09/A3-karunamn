8045e117b8a9c13959c02c52cf6535e120f2cea3
nkaruna09 <nithika.karuna@gmail.com>
2025-01-24 16:54:16 -0500

    Finalize MVP version

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -7,7 +7,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.apache.commons.cli.*; 
 
-public class Main {
+public class Main { 
 
     private static final Logger logger = LogManager.getLogger();
 
