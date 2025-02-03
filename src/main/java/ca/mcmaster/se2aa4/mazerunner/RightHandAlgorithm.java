63107158594f8bb1d82371ce57562b4998b70997
nkaruna09 <nithika.karuna@gmail.com>
2025-02-03 03:16:11 -0500

    revert back to original right hand algorithm

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgorithm.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/RightHandAlgorithm.java
@@ -10,52 +10,36 @@ public class RightHandAlgorithm implements Algorithm {
         Compass compass = new Compass(Direction.E); 
 
         while (!currentPosition.equals(exit)) { 
-            // Try to move in right-hand rule order: right, forward, left, back 
-            if (tryMoveRight(currentPosition, compass, maze, path)) continue; 
-            if (tryMoveForward(currentPosition, compass, maze, path)) continue; 
-            if (tryMoveLeft(currentPosition, compass, maze, path)) continue; 
-            if (tryMoveBackwards(currentPosition, compass, maze, path)) continue; 
-            break; // No valid moves, stop 
+            // Turn right first
+            compass.turnRight();
+            if (currentPosition.stepForward(compass, maze)) {
+                path.append("R").append("F");
+                continue;
+            }
+
+            // If we can't move forward, undo the right turn and move forward
+            compass.turnLeft(); // Undo the right turn
+            if (currentPosition.stepForward(compass, maze)) {
+                path.append("F");
+                continue;
+            }
+
+            // If moving forward isn't possible, turn left
+            compass.turnLeft();
+            if (currentPosition.stepForward(compass, maze)) {
+                path.append("L").append("F");
+                continue;
+            }
+
+            // If all else fails, turn around (turn left twice) and move forward
+            compass.turnLeft();
+            if (currentPosition.stepForward(compass, maze)) {
+                path.append("L").append("L").append("F");
+                continue;
+            }
         }
 
         return path.toString();
     }
-
-    private boolean tryMoveRight(Position position, Compass compass, Maze maze, StringBuilder path) {
-        compass.turnRight();
-        if (position.stepForward(compass, maze)) {
-            path.append("R").append("F");
-            return true;
-        }
-        return false;
-    }
-    
-    private boolean tryMoveForward(Position position, Compass compass, Maze maze, StringBuilder path) {
-        if (position.stepForward(compass, maze)) {
-            path.append("F");
-            return true;
-        }
-        return false;
-    }
-
-    private boolean tryMoveLeft(Position position, Compass compass, Maze maze, StringBuilder path) {
-        compass.turnLeft();
-        if (position.stepForward(compass, maze)) {
-            path.append("L").append("F");
-            return true;
-        }
-        return false;
-    }
-
-    private boolean tryMoveBackwards(Position position, Compass compass, Maze maze, StringBuilder path) {
-        compass.turnLeft(); // Turn around
-        compass.turnLeft(); // Turn around again to face opposite direction
-        if (position.stepForward(compass, maze)) {
-            path.append("L").append("L").append("F");
-            return true;
-        }
-        return false;
-    }
-
 
 }
